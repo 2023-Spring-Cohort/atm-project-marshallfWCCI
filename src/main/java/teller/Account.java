fe15fb840c69404b6a59f8169c31c2e30c580adb
Marshall F <marshall@wecancodeit.org>
2023-01-26 01:41:50 -0500

    Use Code | Generate | Getters

--- src/main/java/teller/Account.java
+++ src/main/java/teller/Account.java
@@ -1,6 +1,18 @@
 package teller;
 
 public class Account {
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public double getAccountBalance() {
+        return accountBalance;
+    }
+
     private final String accountNumber;
     private final String accountType;
     private final double accountBalance;
