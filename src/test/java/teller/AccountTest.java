0390d2c2ae44a1d0f64fdb1ba63ccdab0c89d36c
Marshall F <marshall@wecancodeit.org>
2023-01-26 01:42:37 -0500

    Code | Generate | Tests

---
+++ src/test/java/teller/AccountTest.java
@@ -0,0 +1,20 @@
+package teller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountTest {
+
+    @Test
+    void getAccountNumber() {
+    }
+
+    @Test
+    void getAccountType() {
+    }
+
+    @Test
+    void getAccountBalance() {
+    }
+}
