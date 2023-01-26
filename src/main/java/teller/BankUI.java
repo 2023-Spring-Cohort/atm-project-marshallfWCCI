1f76ab363c42600aaa0279e89aece48ab7749fc5
Marshall F <marshall@wecancodeit.org>
2023-01-26 06:07:16 -0500

    Closing an account

--- src/main/java/teller/BankUI.java
+++ src/main/java/teller/BankUI.java
@@ -14,6 +14,7 @@ public class BankUI {
     public static final String UNEXPECTED_COMMAND = "Only commands 0,1,2,3, or 4 are expected\n";
     public static final String DEPOSIT = "You want to deposit\n";
     public static final String WITHDRAW  = "You want to withdraw\n";
+    public static final String CLOSE  = "You would like to close an account\n";
     public static final String HERE_ARE_YOUR_ACCOUNTS = "Here are your accounts\n";
     public static final String ENTER_ACCOUNT = "Type the account number to choose an account to deposit to.\n";
     public static final String ENTER_DEPOSIT_AMOUNT = "Enter the amount to deposit.\n";
@@ -26,9 +27,9 @@ public class BankUI {
     public static final String YOU_WITHDREW_SUFFIX = "\n";
     public static final String UPDATED_BALANCE = "Your updated balance is now: ";
     public static final String UPDATED_BALANCE_SUFFIX = "\n";
+    public static final String YOU_CLOSED = "You closed: ";
+    public static final String YOU_CLOSED_SUFFIX = "\nHere are your remaining accounts\n";
     public static final String NEXT = "What would you like to do next?";
-    public static final String CLOSING = "You are closing account: ";
-    public static final String CLOSING_SUFFIX = "\nHere are your remaining accounts\n";
     public static final String BYE = "Goodbye ... please come again!\n";
 
     public BankUI(Bank bank, Scanner input, PrintStream output) {
@@ -78,6 +79,20 @@ public class BankUI {
                     bank.getAccount(accountNumber).withdraw(amt);
                     output.print(YOU_WITHDREW + amt + YOU_WITHDREW_SUFFIX);
                     output.print(UPDATED_BALANCE + bank.getAccount(accountNumber).getAccountBalance() + UPDATED_BALANCE_SUFFIX);
+                    output.print(NEXT);
+                    break;
+                }
+                case 4: {
+                    output.print(CLOSE);
+                    output.print(HERE_ARE_YOUR_ACCOUNTS);
+                    displayAccounts();
+                    output.print(ENTER_ACCOUNT);
+                    final String accountNumber = input.next();
+                    output.print(YOU_HAVE_SELECTED + accountNumber + YOU_HAVE_SELECTED_SUFFIX);
+                    bank.closeAccount(accountNumber);
+                    output.print(YOU_CLOSED + accountNumber + YOU_CLOSED_SUFFIX);
+                    output.print(HERE_ARE_YOUR_ACCOUNTS);
+                    displayAccounts();
                     output.print(NEXT);
                     break;
                 }
--- src/test/java/teller/BankUITest.java
+++ src/test/java/teller/BankUITest.java
@@ -90,6 +90,29 @@ class BankUITest {
                         BankUI.BYE);
     }
 
+    @Test
+    void testClosing() {
+        runScenario("4\n" + //
+                        Bank.TEST_ACCOUNT_NUMBER1 + "\n" + //
+                        "0\n", //
+                BankUI.HERE_ARE_YOUR_ACCOUNTS +  //
+                        "(" + Bank.TEST_ACCOUNT_NUMBER1 + ") " + Bank.TEST_ACCOUNT_TYPE1 + " " + Bank.TEST_ACCOUNT_BALANCE1 + "\n" + //
+                        "(" + Bank.TEST_ACCOUNT_NUMBER2 + ") " + Bank.TEST_ACCOUNT_TYPE2 + " " + Bank.TEST_ACCOUNT_BALANCE2 + "\n" + //
+                        BankUI.HELP_MSG + //
+                        BankUI.CLOSE + //
+                        BankUI.HERE_ARE_YOUR_ACCOUNTS + //
+                        "(" + Bank.TEST_ACCOUNT_NUMBER1 + ") " + Bank.TEST_ACCOUNT_TYPE1 + " " + Bank.TEST_ACCOUNT_BALANCE1 + "\n" + //
+                        "(" + Bank.TEST_ACCOUNT_NUMBER2 + ") " + Bank.TEST_ACCOUNT_TYPE2 + " " + Bank.TEST_ACCOUNT_BALANCE2 + "\n" + //
+                        BankUI.ENTER_ACCOUNT + //
+                        BankUI.YOU_HAVE_SELECTED + Bank.TEST_ACCOUNT_NUMBER1 + BankUI.YOU_HAVE_SELECTED_SUFFIX + //
+                        BankUI.YOU_CLOSED + Bank.TEST_ACCOUNT_NUMBER1+ BankUI.YOU_CLOSED_SUFFIX + //
+                        BankUI.HERE_ARE_YOUR_ACCOUNTS + //
+                        "(" + Bank.TEST_ACCOUNT_NUMBER2 + ") " + Bank.TEST_ACCOUNT_TYPE2 + " " + Bank.TEST_ACCOUNT_BALANCE2 + "\n" + //
+                        BankUI.NEXT + //
+                        BankUI.HELP_MSG +  //
+                        BankUI.BYE);
+    }
+
     private static void runScenario(String source, String expected) {
         final Bank bank = new Bank();
         bank.populateWithTestAccount();
