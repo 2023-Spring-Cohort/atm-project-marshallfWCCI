59d34c1c375b3e7dee3bc935c32809008f68969e
Marshall F <marshall@wecancodeit.org>
2023-01-26 01:44:47 -0500

    Populate constructor tests

--- src/test/java/teller/AccountTest.java
+++ src/test/java/teller/AccountTest.java
@@ -8,13 +8,16 @@ class AccountTest {
 
     @Test
     void getAccountNumber() {
+        assertEquals("1-2-3", (new Account("1-2-3", "Checking", 0).getAccountNumber()));
     }
 
     @Test
     void getAccountType() {
+        assertEquals("Checking", (new Account("1-2-3", "Checking", 0).getAccountType()));
     }
 
     @Test
     void getAccountBalance() {
+        assertEquals(0f, (new Account("1-2-3", "Checking", 0).getAccountBalance()));
     }
 }
