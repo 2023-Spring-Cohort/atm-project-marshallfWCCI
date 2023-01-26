612d56809934286c4e11d50ccb308ea86a478a1a
Marshall F <marshall@wecancodeit.org>
2023-01-26 02:14:27 -0500

    Create main()

---
+++ src/main/java/teller/BankUI.java
@@ -0,0 +1,12 @@
+package teller;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class BankUI {
+    public BankUI(Bank bank, Scanner input, PrintStream output) {
+    }
+
+    public void runUntilDone() {
+    }
+}
--- src/main/java/teller/BankingApp.java
+++ src/main/java/teller/BankingApp.java
@@ -1,4 +1,18 @@
 package teller;
 
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class BankingApp {
+    public static void main(String[] args) {
+        final Bank bank = new Bank();
+        bank.openNewAccount(new Account("123-456", "Checking", 100));
+        bank.openNewAccount(new Account("654-321", "Savings", 1000));
+
+        final Scanner input = new Scanner(System.in);
+        final PrintStream output = System.out;
+
+        final BankUI bankUI = new BankUI(bank, input, output);
+        bankUI.runUntilDone();
+    }
 }
