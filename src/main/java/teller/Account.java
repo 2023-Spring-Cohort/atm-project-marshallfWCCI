485654b9a9961395028218538ea4a28863bd279f
Marshall F <marshall@wecancodeit.org>
2023-02-02 07:30:38 -0500

    Move builder into underlying class

--- src/main/java/teller/Account.java
+++ src/main/java/teller/Account.java
@@ -30,4 +30,30 @@ public class Account {
     public void withdraw(double amount) {
         this.accountBalance -= amount;
     }
+
+    // Fluent interface for constructing Account instances
+    public static class AccountBuilder {
+        private String type = "checking";
+        private String accountNumber = "";
+        private double balance = 0;
+
+        public Account build() {
+            return new Account(accountNumber, type, balance);
+        }
+
+        public AccountBuilder withType(String type) {
+            this.type = type;
+            return this;
+        }
+
+        public AccountBuilder withAccountNumber(String s) {
+            this.accountNumber = s;
+            return this;
+        }
+
+        public AccountBuilder withBalance(double balance) {
+            this.balance = balance;
+            return this;
+        }
+    }
 }
