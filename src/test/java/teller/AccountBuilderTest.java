40e25b6aa2941c02ae94f4d87939e10b246512dd
Marshall F <marshall@wecancodeit.org>
2023-02-02 07:27:22 -0500

    Inline builders

--- src/test/java/teller/AccountBuilderTest.java
+++ src/test/java/teller/AccountBuilderTest.java
@@ -4,45 +4,38 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// Note: builders are usually inlined, so we test that way
 class AccountBuilderTest {
     @Test public void testSignature() {
-        final AccountBuilder builder = new AccountBuilder();
-        final Account account = builder.build();
+        final Account account = new AccountBuilder().build();
     }
 
     @Test public void testDefaultBalance() {
-        final AccountBuilder builder = new AccountBuilder();
-        final Account account = builder.build();
+        final Account account = new AccountBuilder().build();
         assertEquals(0, account.getAccountBalance());
     }
     @Test public void testDefaultNumber() {
-        final AccountBuilder builder = new AccountBuilder();
-        final Account account = builder.build();
+        final Account account = new AccountBuilder().build();
         assertEquals("", account.getAccountNumber());
     }
     @Test public void testDefaultType() {
-        final AccountBuilder builder = new AccountBuilder();
-        final Account account = builder.build();
+        final Account account = new AccountBuilder().build();
         assertEquals("checking", account.getAccountType());
     }
     @Test public void testSavingsType() {
-        final AccountBuilder builder = new AccountBuilder();
-        final Account account = builder.withType("savings").build();
+        final Account account = new AccountBuilder().withType("savings").build();
         assertEquals("savings", account.getAccountType());
     }
     @Test public void testAccountNumber() {
-        final AccountBuilder builder = new AccountBuilder();
-        final Account account = builder.withAccountNumber("555-1234").build();
+        final Account account = new AccountBuilder().withAccountNumber("555-1234").build();
         assertEquals("555-1234", account.getAccountNumber());
     }
     @Test public void testBalance() {
-        final AccountBuilder builder = new AccountBuilder();
-        final Account account = builder.withBalance(500).build();
+        final Account account = new AccountBuilder().withBalance(500).build();
         assertEquals(500, account.getAccountBalance());
     }
     @Test public void testAll() {
-        final AccountBuilder builder = new AccountBuilder();
-        final Account account = builder.withType("special").withAccountNumber("555-5555").withBalance(1000.5).build();
+        final Account account = new AccountBuilder().withType("special").withAccountNumber("555-5555").withBalance(1000.5).build();
         assertEquals("555-5555", account.getAccountNumber());
         assertEquals("special", account.getAccountType());
         assertEquals(1000.5, account.getAccountBalance());
