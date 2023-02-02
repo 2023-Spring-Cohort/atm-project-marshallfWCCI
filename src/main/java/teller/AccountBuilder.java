c0ac916d0f9f5ca10f84229ee74de8cbf447e60c
Marshall F <marshall@wecancodeit.org>
2023-02-02 07:09:31 -0500

    testDefaultNumber

--- src/main/java/teller/AccountBuilder.java
+++ src/main/java/teller/AccountBuilder.java
@@ -3,6 +3,6 @@ package teller;
 // Fluent interface for constructing Account instances
 public class AccountBuilder {
     public Account build() {
-        return new Account(null, null, 0);
+        return new Account("", null, 0);
     }
 }
--- src/test/java/teller/AccountBuilderTest.java
+++ src/test/java/teller/AccountBuilderTest.java
@@ -15,4 +15,9 @@ class AccountBuilderTest {
         final Account account = builder.build();
         assertEquals(0, account.getAccountBalance());
     }
+    @Test public void testDefaultNumber() {
+        final AccountBuilder builder = new AccountBuilder();
+        final Account account = builder.build();
+        assertEquals("", account.getAccountNumber());
+    }
 }
